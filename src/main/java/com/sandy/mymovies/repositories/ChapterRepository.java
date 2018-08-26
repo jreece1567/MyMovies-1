5d68421cae2e47b05bf077b4329d9cf46d841a72
John Reece <jreece1567@yahoo.com>
2018-08-26 02:09:33 -0700

    Typo.

--- src/main/java/com/sandy/mymovies/repositories/ChapterRepository.java
+++ src/main/java/com/sandy/mymovies/repositories/ChapterRepository.java
@@ -14,14 +14,10 @@ public interface ChapterRepository extends CrudRepository<Chapter, String> {
   Optional<Chapter> findByImdbIdAndSeasonAndEpisodeNumber(String imdbId, Integer season,
       Integer episodeNumber);
 
-  // /episodes/{imdbId}
   List<Chapter> findAllByImdbId(String imdbId);
 
-  // /episodes/{imdbId}/{season}
   List<Chapter> findAllByImdbIdAndSeason(String imdbId, Integer season);
 
-  // todo: ??
-  // /seasons/{imdbId}
   @Query("SELECT DISTINCT season FROM Chapter WHERE imdbId=?1")
   List<String> findDistinctSeasonsByImdbId(String imdbId);
 }
