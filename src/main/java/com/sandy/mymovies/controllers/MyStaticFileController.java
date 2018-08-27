d83f40d1a7475ecbb1bd2a58cafbdfc40fe470bc
John Reece <jreece1567@yahoo.com>
2018-08-27 01:16:48 -0700

    Minor tweaks.

---
+++ src/main/java/com/sandy/mymovies/controllers/MyStaticFileController.java
@@ -0,0 +1,55 @@
+package com.sandy.mymovies.controllers;
+
+import com.sandy.mymovies.services.MyStaticFileService;
+import java.util.NoSuchElementException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class MyStaticFileController {
+
+  MyStaticFileService staticFileService;
+
+  @Autowired
+  public MyStaticFileController(MyStaticFileService staticFileService) {
+
+    this.staticFileService = staticFileService;
+  }
+
+  /**
+   * Exception handler if NoSuchElementException is thrown
+   *
+   * @param ex the thrown exception.
+   * @return Error message String.
+   */
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  @ExceptionHandler(NoSuchElementException.class)
+  public String return404(final NoSuchElementException ex) {
+
+    return ex.getMessage();
+  }
+
+  /**
+   * Fetch the poster-image bytes for a given imdbId.
+   *
+   * @param imdbId the unique IMDB-id of the movie.
+   * @return the image-bytes, with appropriate content-type and content-length headers.
+   */
+  @RequestMapping(path = "/image/{imdbId}", method = RequestMethod.GET)
+  @ResponseStatus(HttpStatus.OK)
+  public ResponseEntity<byte[]> fetchImageForImdbId(@PathVariable("imdbId") String imdbId) {
+
+    byte[] imageBytes = staticFileService.fetchPosterImage(imdbId);
+
+    return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).contentLength(imageBytes.length)
+        .body(imageBytes);
+  }
+}
---
+++ src/main/java/com/sandy/mymovies/services/MyStaticFileService.java
@@ -0,0 +1,52 @@
+package com.sandy.mymovies.services;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.NoSuchElementException;
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Service;
+
+@Log
+@Service
+public class MyStaticFileService {
+
+  /**
+   * Read and return the poster-image bytes for given imdbId.
+   *
+   * @param imdbId the unique IMDB-id of the movie.
+   * @return the image bytes.
+   */
+  public byte[] fetchPosterImage(String imdbId) {
+
+    final String filename = "/db/" + imdbId + ".jpg";
+    final InputStream imageInput = getClass().getResourceAsStream(filename);
+    if (imageInput == null) {
+      throw new NoSuchElementException(filename + " not found.");
+    }
+
+    final BufferedInputStream imageReader =
+        new BufferedInputStream(imageInput);
+    final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+    byte[] buffer = new byte[8192];
+    try {
+
+      int len = 0;
+      do {
+        len = imageReader.read(buffer);
+        if (len != -1) {
+          baos.write(buffer, 0, len);
+        }
+      } while (len != -1);
+
+    } catch (IOException ex) {
+      log.info("Error reading image from " + filename + " - " + ex.getMessage());
+      throw new NoSuchElementException("Error reading image from " + filename);
+    }
+
+    return baos.toByteArray();
+  }
+
+}
