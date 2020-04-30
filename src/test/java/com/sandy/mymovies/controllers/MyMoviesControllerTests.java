466cacbddbecb9402b6b35a4c27958b22cf00f46
John Reece <jreece1567@yahoo.com>
2020-04-30 02:23:55 -0700

    Add controller tests.

--- src/test/java/com/sandy/mymovies/controllers/MyMoviesControllerTests.java
+++ src/test/java/com/sandy/mymovies/controllers/MyMoviesControllerTests.java
@@ -1,62 +1,85 @@
-//package com.sandy.mymovies.controllers;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//import com.sandy.mymovies.MyMoviesApplication;
-//import com.sandy.mymovies.repositories.ActorRepository;
-//import com.sandy.mymovies.repositories.ChapterRepository;
-//import com.sandy.mymovies.repositories.GenreRepository;
-//import com.sandy.mymovies.repositories.TagRepository;
-//import com.sandy.mymovies.repositories.VideoRepository;
-//import junit.framework.TestCase;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = MyMoviesApplication.class)
-//@AutoConfigureMockMvc
-//public class MyMoviesControllerTests {
-//
-//  @Autowired
-//  private MockMvc mockMvc;
-//
-//  @MockBean
-//  ActorRepository actorRepository;
-//
-//  @MockBean
-//  ChapterRepository chapterRepository;
-//
-//  @MockBean
-//  GenreRepository genreRepository;
-//
-//  @MockBean
-//  TagRepository tagRepository;
-//
-//  @MockBean
-//  VideoRepository videoRepository;
-//
-//  /**
-//   * See if we can fetch a movie.
-//   */
-//  @Test
-//  public void fetchMovie_returnsMovie() {
-//
-//    try {
-//      mockMvc.perform(get("/movie/0128442").contentType(MediaType.APPLICATION_JSON))
-//          .andExpect(status().isOk());
-//    } catch (Exception ex) {
-//      TestCase.fail(ex.getMessage());
-//    }
-//
-//
-//  }
-//
-//}
+package com.sandy.mymovies.controllers;
+
+import static junit.framework.TestCase.fail;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.sandy.mymovies.MyMoviesApplication;
+import com.sandy.mymovies.repositories.ActorRepository;
+import com.sandy.mymovies.repositories.ChapterRepository;
+import com.sandy.mymovies.repositories.GenreRepository;
+import com.sandy.mymovies.repositories.TagRepository;
+import com.sandy.mymovies.repositories.VideoRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MyMoviesApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(
+    locations = "classpath:application-integrationTest.properties")
+public class MyMoviesControllerTests {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  ActorRepository actorRepository;
+
+  @Autowired
+  ChapterRepository chapterRepository;
+
+  @Autowired
+  GenreRepository genreRepository;
+
+  @Autowired
+  TagRepository tagRepository;
+
+  @Autowired
+  VideoRepository videoRepository;
+
+  /**
+   * See if we can fetch a movie.
+   */
+  @Test
+  public void fetchMovie_returnsMovie() {
+
+    try {
+      mockMvc.perform(get("/movie/0128442").contentType(MediaType.APPLICATION_JSON))
+          .andExpect(status().isOk())
+          .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+          .andExpect(jsonPath("title", is("Rounders")));
+    } catch (Exception ex) {
+      fail(ex.getMessage());
+    }
+
+  }
+
+  /**
+   * See if we can fetch an index.
+   */
+  @Test
+  public void fetchIndex_returnsIndex() {
+
+    try {
+      mockMvc.perform(get("/index/keys/all").contentType(MediaType.APPLICATION_JSON))
+          .andExpect(status().isOk())
+          .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+          .andExpect(jsonPath("$[6]", is("title")));
+    } catch (Exception ex) {
+      fail(ex.getMessage());
+    }
+
+  }
+
+}
--- src/test/java/com/sandy/mymovies/controllers/MyMoviesStaticFileControllerTests.java
+++ src/test/java/com/sandy/mymovies/controllers/MyMoviesStaticFileControllerTests.java
@@ -1,50 +1,52 @@
-//package com.sandy.mymovies.controllers;
-//
-//import static junit.framework.TestCase.fail;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//import com.sandy.mymovies.MyMoviesApplication;
-//import com.sandy.mymovies.services.MyStaticFileService;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = MyMoviesApplication.class)
-//@AutoConfigureMockMvc
-//public class MyMoviesStaticFileControllerTests {
-//
-//  @Autowired
-//  private MockMvc mockMvc;
-//
-//  @MockBean
-//  MyStaticFileService staticFileService;
-//
-//  @Test
-//  public void fetchPosterImageWithValidImdbId_returnsImage() {
-//    try {
-//      mockMvc.perform(get("/image/0128442").contentType(MediaType.IMAGE_JPEG))
-//          .andExpect(status().isOk());
-//    } catch (Exception ex) {
-//      fail(ex.getMessage());
-//    }
-//  }
-//
-//  @Test
-//  public void fetchPosterImageWithInvalidImdbId_returnsError() {
-//    try {
-//      mockMvc.perform(get("/image/0999999").contentType(MediaType.IMAGE_JPEG))
-//          .andExpect(status().isNotFound());
-//    } catch (Exception ex) {
-//      fail(ex.getMessage());
-//    }
-//  }
-//
-//}
+package com.sandy.mymovies.controllers;
+
+import static junit.framework.TestCase.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.sandy.mymovies.MyMoviesApplication;
+import com.sandy.mymovies.services.MyStaticFileService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MyMoviesApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(
+    locations = "classpath:application-integrationTest.properties")
+public class MyMoviesStaticFileControllerTests {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  MyStaticFileService staticFileService;
+
+  @Test
+  public void fetchPosterImageWithValidImdbId_returnsImage() {
+    try {
+      mockMvc.perform(get("/image/0128442").contentType(MediaType.IMAGE_JPEG))
+          .andExpect(status().isOk());
+    } catch (Exception ex) {
+      fail(ex.getMessage());
+    }
+  }
+
+  @Test
+  public void fetchPosterImageWithInvalidImdbId_returnsError() {
+    try {
+      mockMvc.perform(get("/image/0999999").contentType(MediaType.IMAGE_JPEG))
+          .andExpect(status().isNotFound());
+    } catch (Exception ex) {
+      fail(ex.getMessage());
+    }
+  }
+
+}
