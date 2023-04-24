6d292e4f487dd6e155be493a0f24b79859253623
John Reece <jreece1567@yahoo.com>
2023-04-23 20:47:29 -0700

    Add tests.

--- src/test/java/com/sandy/mymovies/MyMoviesTestData.java
+++ src/test/java/com/sandy/mymovies/MyMoviesTestData.java
@@ -3,12 +3,12 @@ package com.sandy.mymovies;
 public final class MyMoviesTestData {
 
   public static final Integer ALL_COUNT = 8;
-  public static final Integer ACTOR_COUNT = 94264;
-  public static final Integer DIRECTOR_COUNT = 867;
+  public static final Integer ACTOR_COUNT = 94309;
+  public static final Integer DIRECTOR_COUNT = 868;
   public static final Integer GENRE_COUNT = 25;
   public static final Integer RATING_COUNT = 15;
   public static final Integer TAG_COUNT = 28;
-  public static final Integer TITLE_COUNT = 1805;
+  public static final Integer TITLE_COUNT = 1808;
   public static final Integer YEAR_COUNT = 93;
 
   public static final Integer TEST_ACTOR_COUNT = 26;
